a488292c80d19dd73d95a38c69d7c14fe87cd27d
Charles de Freitas <me@chasbob.dev>
2021-03-13 14:07:43 +0000

    Add optional support for monorepos

--- script.sh
+++ script.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+function golangci-multi-module() {
+  # git ref to check 
+  FILE_FILTER="go$"
+  if [[ ${INPUT_MULTI_MODULE} ]]
+  then
+    # shellcheck disable=SC2086
+    golangci-lint run --out-format line-number ${INPUT_GOLANGCI_LINT_FLAGS}
+    return
+  fi
+  FILES=$(git diff-tree --no-commit-id --name-only -r "${GITHUB_SHA}"| grep "${FILE_FILTER}")
+  MODULES=$(find . -type f -name 'go.mod' -exec dirname {} \; 2>/dev/null | cut -c 3- | sort | uniq)
+  
+  for m in $MODULES; do
+    PKGs=$(echo "$FILES" | grep "^$m" | cut -d '/' -f2- | xargs -I ff dirname ff | sort | uniq | tr '\n' ' ')
+    if [[ ! "$PKGs" == '' ]]; then
+      pushd "$m" >/dev/null || continue
+      # shellcheck disable=SC2086
+      golangci-lint run --out-format line-number ${GOLANGCI_LINT_FLAGS} --path-prefix="$m" $PKGs
+      popd >/dev/null || continue
+    fi
+  done
+}
+
 cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
 
 TEMP_PATH="$(mktemp -d)"
@@ -17,7 +40,7 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 echo '::group:: Running golangci-lint with reviewdog 🐶 ...'
 # shellcheck disable=SC2086
-golangci-lint run --out-format line-number ${INPUT_GOLANGCI_LINT_FLAGS} \
+golangci-multi-module \
   | reviewdog -f=golangci-lint \
       -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
